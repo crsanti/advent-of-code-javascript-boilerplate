c4846271ea0f2598a25591c31e393cddc2e878b0
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2021-08-16 18:14:11 +0200

    feat(day07): added day07

---
+++ src/day07/part2.test.js
@@ -0,0 +1,17 @@
+const solver = require("./part2")
+
+describe.skip("Day 07 > part 2", () => {
+  test("'a' should have 1968", () => {
+    const input = [
+      ["123 -> b"],
+      ["456 -> y"],
+      ["b AND y -> d"],
+      ["b OR y -> e"],
+      ["b LSHIFT 2 -> a"],
+      ["y RSHIFT 2 -> g"],
+      ["NOT b -> h"],
+      ["NOT y -> i"],
+    ].join("\n")
+    expect(solver(input)).toBe(1968)
+  })
+})
