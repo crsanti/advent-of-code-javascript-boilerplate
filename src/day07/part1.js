7218ea2194509bd814139e065d169bd74a315d0a
Santiago Camargo Rodríguez <santi.camargo.rodriguez@gmail.com>
2021-08-19 15:24:55 +0200

    fix(day07): fixed day07 part2

--- src/day07/part1.js
+++ src/day07/part1.js
@@ -1,9 +1,3 @@
 module.exports = (input) => {
   throw new Error("To be implemented")
 }
-
-// Helper to be called at last instance to retrieve the value of 'a'.
-// It is not tied to any data structure.
-const getA = (data) => {
-  throw new Error("To be implemented")
-}
--- src/day07/part2.js
+++ src/day07/part2.js
@@ -1,18 +1,3 @@
-const solver = require("./part1")
-
-// This uses part1 to retrieve the value of 'a' and prepare the input for part2
-const prepareInput = (input) => {
-  const a_value = solver(input)
-  return input.replace(/\d+ -> b\n/, `${a_value} -> b\n`)
-}
-
 module.exports = (input) => {
-  input = prepareInput(input)
-  throw new Error("To be implemented")
-}
-
-// Helper to be called at last instance to retrieve the value of 'a'.
-// It is not tied to any data structure.
-const getA = (data) => {
   throw new Error("To be implemented")
 }
